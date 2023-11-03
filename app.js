e0e7e8133e0655292e053a1ce85adb9bb97f47e9
usamajaved4216 <usamajaved4216@gmail.com>
2023-11-04 01:28:03 +0500

    complete

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+                      //border table
+// function generateTable() {
+//     var number = parseInt(document.getElementById('numberInput').value);
+//     if (!isNaN(number)) {
+//         var table = "<table border='1'><tr><th>Number</th><th>Multiplication Result</th></tr>";
+//         for (var i = 1; i <= 10; i++) {
+//             var result = number * i;
+//             table += "<tr><td>" + number + " x " + i + "</td><td>" + result + "</td></tr>";
+//         }
+//         table += "</table>";
+//         document.getElementById('tableContainer').innerHTML = table;
+//     } else {
+//         document.getElementById('tableContainer').innerHTML = "Invalid input. Please enter a valid number.";
+//     }
+// }
 
-function generateTable() {
-    var number = parseInt(document.getElementById('numberInput').value);
-    if (!isNaN(number)) {
-        var table = "<table border='1'><tr><th>Number</th><th>Multiplication Result</th></tr>";
-        for (var i = 1; i <= 10; i++) {
-            var result = number * i;
-            table += "<tr><td>" + number + " x " + i + "</td><td>" + result + "</td></tr>";
-        }
-        table += "</table>";
-        document.getElementById('tableContainer').innerHTML = table;
-    } else {
-        document.getElementById('tableContainer').innerHTML = "Invalid input. Please enter a valid number.";
-    }
-}
+                             // without border table
+// function generateTable() {
+//     var number = parseInt(document.getElementById('numberInput').value);
+//     if (!isNaN(number)) {
+//         var table = '<br>'
+//         for (var i = 1; i <= 10; i++) {
+//             var result = number * i;
+//             table += number + " x " + i + ' = ' + result + '<br>';
+//         }
+        
+//         document.getElementById('tableContainer').innerHTML = table;
+//     } else {
+//         document.getElementById('tableContainer').innerHTML = "Invalid input. Please enter a valid number.";
+//     }
+// }
